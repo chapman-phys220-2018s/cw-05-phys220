e51fd638280c7608efdd597fc097cecb9322dad4
Justin Dressel <dressel@chapman.edu>
2017-09-26 08:11:41 +0000

    Dummy test added

---
+++ test_cw05.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""CW05 Test Module
+
+As all classwork this week is in the notebook, this test file
+will only test whether you hook your repository README into Travis-ci
+correctly, for practice.
+"""
+
+def test_dummy():
+    """Dummy test that always passes."""
+    assert True
